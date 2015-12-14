44409952bd0ea054cfbc413d6974c8cafcfe573e
Jung HeeChang <jgmlckd@gmail.com>
2015-12-14 17:06:10 +0900

    숏컷 다시 추가함~~

---
+++ SCItem.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+namespace ShortCutItem
+{
+    public class SCItem : MonoBehaviour
+    {
+        static int numItem = 0;
+        private int groupId;
+        private int itemId;
+        private string itemName;
+
+        public SCItem()
+        {
+            this.groupId = 0;
+            this.itemId = 0;
+            string itemName="";
+            numItem++;
+        }
+
+        public SCItem(int groupid, int itemid, string itemName)
+        {
+            this.groupId = groupid;
+            this.itemId = itemid;
+            this.itemName = itemName;
+            numItem++;
+        }
+
+        public void setGroupId(int groupid)
+        {
+            this.groupId = groupid;  
+        }
+
+        public void setItemId(int itemid)
+        {
+            this.itemId = itemid;
+        }
+
+        public void setItemName(string itemName)
+        {
+            this.item = itemName;
+        }
+
+        public int getGroupId()
+        {
+            return this.groupId;
+        }
+
+        public int getId()
+        {
+            return this.itemId;
+        }
+
+        public string getItemName()
+        {
+            return this.itemName;
+        }
+
+        ~SCItem() { }
+
+    }
+}
---
+++ main.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Leap;
+using System.Collections;
+using ShortCutAdapter;
+using ShortCutItem;
+using ShortCutView;
+
+
+public class main : MonoBehaviour
+{
+   
+    public Vector3 _iniPos;
+    public Vector3 _nowPos;
+    public Controller controller;
+    public  Frame frame;
+    public FingerList fingerlist;
+    public Finger finger;
+    public GameObject shortCutView;
+    public GameObject trakedCamera;
+    public HandController handContorller;
+    public string name;
+    public Hand hand;
+    public Arm arm;
+    public int num;
+    public bool IsLeftSide;
+    //통합 과정
+    private SCItem scitem1;
+    private SCItem scitem2;
+    private SCItem scitem3;
+    private SCAdapter scadapter;
+    private SCView scview;
+    private GameObject ob;
+   
+
+	// Use this for initialization
+	void Start () {
+        //통합 과정
+        scadapter = new SCAdapter();
+        scitem1 = new SCItem(0, 1, "개 같은");
+        scitem2 = new SCItem(0, 3, "창과");
+        scitem3 = new SCItem(0, 2, "극혐");
+
+        scadapter.addItem(scitem1, "Button-Square-Text-OFF(3-1)");
+        scadapter.addItem(scitem2, "Button-Square-Text-OFF(3-2)");
+        scadapter.addItem(scitem3, "Button-Square-Text-OFF(3-3)");
+
+        scview = new SCView(scadapter);
+        scview.setBtnSize(2f);
+        scview.setTextSize(30);
+        scview.setTextColor(Color.red);
+        scview.onDraw();
+        //scview.transform.position = go2.transform.position;
+        _iniPos = transform.position;
+         controller = new Controller();
+
+
+        /* 이벤트 등록 부분 */
+        /*
+         * 
+         * 
+         * 
+         */
+        
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+        shortCutView = GameObject.Find(name);
+        
+        
+        if (num == 1)
+        {
+           frame = controller.Frame(0);
+           hand = frame.Hands.Frontmost;
+           if(hand.IsValid)
+           {
+             
+               if (IsLeftSide)
+               {
+                   if (hand.IsLeft)
+                   {
+                       arm = hand.Arm;
+                       Vector wrist = arm.WristPosition;
+                       // print(arm.ToString());
+                       Vector3 unityPosition = UnityVectorExtension.ToUnityScaled(wrist, false);
+
+                       Vector3 worldPosition = handContorller.transform.TransformPoint(unityPosition);
+
+                       shortCutView.transform.position = worldPosition + new Vector3(2f, 0, 0);
+                   }
+               }
+               else
+               {
+                   if (hand.IsRight)
+                   {
+                       arm = hand.Arm;
+                       Vector wrist = arm.WristPosition;
+                       // print(arm.ToString());
+                       Vector3 unityPosition = UnityVectorExtension.ToUnityScaled(wrist, false);
+
+                       Vector3 worldPosition = handContorller.transform.TransformPoint(unityPosition);
+
+                       shortCutView.transform.position = worldPosition + new Vector3(-6f, 0, 0);
+                   }
+               }
+           }
+           else
+           {
+               shortCutView.transform.position = trakedCamera.transform.position;
+           }
+        }
+        else if(num==0)
+        {
+            shortCutView.active = true;
+            shortCutView.transform.position = _iniPos + trakedCamera.transform.position + new Vector3(-3f, 0, 5f);
+        }
+        
+	}
+   
+}
